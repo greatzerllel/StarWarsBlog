85ec86a00134785ebe4f6bc6f995877e497dcb21
ZERLLEL <greatzerllel2@gmail.com>
2022-12-05 14:11:20 +0000

    Blog

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,69 @@
-const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+const getState = ({ getStore, setStore }) => {
+    return {
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+        store: {
+            personajes: null,
+            planetas: null,
+            vehiculos: null,
+            favoritos: [],
+            personaje: null,
+            planeta: null,
+            vehiculo: null
+        },
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
-};
+        actions: {
+            getPersonajes: (url) => {
+                fetch(url)
+                    .then((response) => response.json())
+                    .then((data) => {
+                        setStore({ personajes: data })
+                    })
+            },
+            getPersonajeById: (url) => {
+                fetch(url)
+                    .then((response) => response.json())
+                    .then((data) => {
+                        setStore({ personaje: data })
+                    })
+            },
+            getPlanetas: (url) => {
+                fetch(url)
+                    .then((response) => response.json())
+                    .then((data) => {
+                        setStore({ planetas: data })
+                    })
+            },
+            getPlanetaById: (url) => {
+                fetch(url)
+                    .then((response) => response.json())
+                    .then((data) => {
+                        setStore({ planeta: data })
+                    })
+            },
+            getVehiculos: (url) => {
+                fetch(url)
+                    .then((response) => response.json())
+                    .then((data) => {
+                        setStore({ vehiculos: data })
+                    })
+            },
+            getVehiculoById: (url) => {
+                fetch(url)
+                    .then((response) => response.json())
+                    .then((data) => {
+                        setStore({ vehiculo: data })
+                    })
+            },
+            agregarFavoritos: (favorito) => {
+                const { favoritos } = getStore();
+                const favoritosActualizado = [favorito, ...favoritos];
+                setStore({ favoritos: favoritosActualizado });
+
+            }
+
+        }
+
+    }
+}
 
 export default getState;
