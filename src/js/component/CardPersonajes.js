4e66f61e9cee19cabf39bf89bc6ac11aec98ad45
ZERLLEL <greatzerllel2@gmail.com>
2022-12-05 14:14:29 +0000

    Blog

---
+++ src/js/component/CardPersonajes.js
@@ -0,0 +1,32 @@
+import React, {useContext } from "react";
+import { Link } from "react-router-dom";
+import { AppContext } from "../store/appContext";
+
+const CardPersonajes = ({name, uid}) => {
+  
+  const {actions} = useContext(AppContext);
+
+  return (
+    <div className="card" id="cartas">
+      <img src="https://th.bing.com/th/id/R.1bcbc98e5ba7cf0e3e0fd19f4d8b6d09?rik=YRdsKmL5SwuGyQ&riu=http%3a%2f%2fwww.muraldecal.com%2fen%2fimg%2fas311eng-jpg%2ffolder%2fproducts-listado-merchanthover%2fwall-stickers-may-the-force-be-with-you.jpg&ehk=uCwkW5badf31GJONE0bv4iokYpE3AOmEfxU9wUHi3lY%3d&risl=&pid=ImgRaw&r=0" className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <Link
+          to={`personaje/${uid}/detail`}
+          className="btn btn-dark"
+          id="boton-personajes"
+        >
+          Ver más
+        </Link>
+        <button type="button" className="add-favoritos btn btn-light" onClick={() => actions.agregarFavoritos({uid, name, type:"personaje"})}>Add Favoritos</button>
+
+      </div>
+    </div>
+  );
+};
+
+export default CardPersonajes;
--- src/js/layout.js
+++ src/js/layout.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
-
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
 import injectContext from "./store/appContext";
+import  Navbar  from "./component/navbar";
+import Personajes from "./views/Personajes";
+import Footer from "./component/Footer";
+import Vehiculos from "./views/Vehiculos";
+import Planetas from "./views/Planetas";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
 
 //create your first component
 const Layout = () => {
@@ -23,11 +23,12 @@ const Layout = () => {
 					<Navbar />
 					<Routes>
 						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
+						<Route path="/personajes" element={<Personajes />} />
+						<Route path="/vehiculos" element={<Vehiculos />} />
+						<Route path="/planetas" element={<Planetas />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
-					<Footer />
+					<Footer/>
 				</ScrollToTop>
 			</BrowserRouter>
 		</div>
